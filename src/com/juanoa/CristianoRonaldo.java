ecdb7ad75a8c8a2d29017e9ca5e93d57eacb5d8b
Sebastián Osorio <65676087+juanoa26@users.noreply.github.com>
2022-12-19 11:53:31 -0500

    Initial Commit

---
+++ src/com/juanoa/CristianoRonaldo.java
@@ -0,0 +1,12 @@
+package com.juanoa;
+
+public class CristianoRonaldo extends JugadoresDeFutbol{
+
+    public static void main(String[] args) {
+        CristianoRonaldo cr7 = new CristianoRonaldo();
+
+        cr7.correr();
+        System.out.println(cr7.altura = 1.87);
+        System.out.println(cr7.edad = 37);
+    }
+}
---
+++ src/com/juanoa/JugadoresDeFutbol.java
@@ -0,0 +1,15 @@
+package com.juanoa;
+
+public class JugadoresDeFutbol {
+
+    //estado -> variables
+    int edad;
+    double altura;
+
+    //comportamiento -> metodo
+    void correr() {
+        //logica del código
+        System.out.println("El jugador está corriendo");
+    }
+
+}
