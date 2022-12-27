4ebc543ad6bd30f084232bbee10874c698fcc0cf
Sebastián Osorio <65676087+juanoa26@users.noreply.github.com>
2022-12-27 11:47:44 -0500

    Agregando polimorfismo

--- src/com/juanoa/CristianoRonaldo.java
+++ src/com/juanoa/CristianoRonaldo.java
@@ -2,10 +2,16 @@ package com.juanoa;
 
 public class CristianoRonaldo extends JugadoresDeFutbol{
 
+    @Override
+    void correr(boolean tieneElBalon) {
+        System.out.println("Cristiano corre mas rápido");
+    }
+
     public static void main(String[] args) {
         CristianoRonaldo cr7 = new CristianoRonaldo();
 
         cr7.correr();
+        cr7.correr(true);
         System.out.println(cr7.altura = 1.87);
         System.out.println(cr7.edad = 37);
     }
